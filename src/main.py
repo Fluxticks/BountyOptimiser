7c1ff076b624d25a70e511ebbbbb18c40b1c9061
Benji Garment <benjigarment.appdev@gmail.com>
2020-04-06 18:41:17 +0100

    Setup main for normal script running

--- src/main.py
+++ src/main.py
@@ -1,11 +1,12 @@
 from process import Optimise
 from api import API
+from util import makeColorLog, makeLogger
 
 if __name__ == '__main__':
+
+    logger = makeColorLog('MAIN')
     api = API('731b7c448f4f41fdaaef9d95284d7242')
     manifest = api.manifestUpdate('en')
-    print(manifest)
+    logger.info('Manifest is on ver %s', manifest)
     opt = Optimise('731b7c448f4f41fdaaef9d95284d7242', manifest)
-    #opt.OptimiseBounties('Fuxticks')
-    data = opt.getValueFromTable('432848324', 'DestinyInventoryItemDefinition')
-    print(data)
+    opt.OptimiseBounties('Fuxticks')
