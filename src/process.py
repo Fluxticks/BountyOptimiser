09f247dabf931fe21e807d30c7572c7479f90929
Fluxticks <benjigarment.appdev@gmail.com>
2020-04-13 18:12:49 +0100

    Cleaned process.py ready for update

--- src/process.py
+++ src/process.py
@@ -23,30 +23,39 @@ class Optimise():
 
     def makeBuckets(self):
         
-        self.weaponBuckets = ['auto rifle', 'scout rifle', 'hand cannon', 'sniper', 'sidearm', 'pulse rifle', 'submachine gun', 'shotgun', 'fusion rifle', 'trace rifle', 'grenade launcher', 'rocket launcher', 'linear fusion rifle', 'sword', 'heavy machine gun']
-        self.enemyBuckets = ['fallen', 'vex', 'hive', 'taken', 'scorn', 'cabal', 'guardians'] #
-        self.enemyWording = ['opponents', 'combatants', 'enemies', 'any target'] #
-        self.elementBuckets = ['solar', 'void', 'arc'] #
-        self.slotBuckets = ['kinetic', 'energy', 'power', 'ability']
-        self.killBuckets = ['rapid', 'precision', 'multi'] #
-        self.abilityBuckets = ['melee', 'super', 'grenade']
-        self.planetBuckets = ['edz', 'titan', 'moon', 'nessus', 'tangled shore', 'dreaming city', 'mercury', ' io', 'mars']
+        weapons = ['auto rifle', 'scout rifle', 'hand cannon', 'sniper', 'sidearm', 'pulse rifle', 'submachine gun', 'shotgun', 'fusion rifle', 'trace rifle', 'grenade launcher', 'rocket launcher', 'linear fusion rifle', 'sword', 'heavy machine gun']
+        combatants = ['fallen', 'vex', 'hive', 'taken', 'scorn', 'cabal', 'guardians'] 
+        targets = ['opponents', 'combatants', 'enemies', 'any target'] 
+        elements = ['solar', 'void', 'arc'] 
+        slots = ['kinetic', 'energy', 'power', 'ability']
+        killType = ['rapid', 'precision', 'multi'] 
+        abilities = ['melee', 'super', 'grenade']
+        planets = ['edz', 'titan', 'moon', 'nessus', 'tangled shore', 'dreaming city', 'mercury', ' io', 'mars']
+        activities = ['crucible', 'gambit', 'strike', 'nightfall', 'raid']
+
+        self.Tier0 = slots
+
+        self.Tier1 = weapons + abilities
+
+        self.Tier2 = elements + killType + targets
+
+        self.Tier3 = combatants
+
+        self.Tier4 = planets
+
+        self.Tier5 = activities
 
         #Mutually Exclusive buckets mean the objective in the same bucket can't happen at the same time
-        exclusiveBuckets = [self.weaponBuckets, self.enemyBuckets, self.elementBuckets, self.slotBuckets, self.abilityBuckets, self.planetBuckets]
-
-        self.xorBuckets = [self.weaponBuckets, self.abilityBuckets]
+        exclusiveBuckets = [weapons, combatants, elements, slots, abilities, planets]
 
         self.exclusiveData = {}
 
         for bucket in exclusiveBuckets:
-            self.exclusiveData.update(self.makeExclusive(bucket))
+            self.exclusiveData.update(makeExclusive(bucket))
 
-        self.exclusiveData['kinetic'] += self.elementBuckets
+        self.exclusiveData['kinetic'] += elements
 
-        #dprint(self.exclusiveData)
-
-        self.baseBuckets = self.weaponBuckets + self.enemyBuckets + self.enemyWording + self.elementBuckets + self.slotBuckets + self.killBuckets + self.abilityBuckets + self.planetBuckets
+        #dprint(exclusiveData)
 
 
     def makeExclusive(self, items):
@@ -55,119 +64,6 @@ class Optimise():
             data[items[i]] = items[:i] + items[i+1:]
         return data
 
-
-    def getGeneralBuckets(self, objective):
-
-        buckets = []
-        objective = objective.lower()
-
-        for bucket in self.baseBuckets:
-            if bucket in objective:
-                buckets.append(bucket)
-
-        if 'generate orbs' in objective or 'orbs created' in objective:
-            if 'super' not in buckets:
-                buckets.append('orbs')
-
-        if 'collect orbs' in objective or 'orbs collected' in objective:
-            buckets.append('masterwork')
-
-        return buckets
-
-
-    def getCrucibleBuckets(self, objective):
-        
-        crucibleBuckets = ['win', 'crucible', 'matches']
-        modesBuckets = ['clash', 'control', 'rumble', 'skirmish', 'supremacy', 'breakthrough', 'countdown', 'survival', 'iron banner']
-        objectiveBasedBuckets = {'supremacy':'crests', 'control':'zones', 'countdown':'charges'}
-
-        objective = objective.lower()
-
-        buckets = self.getGeneralBuckets(objective)
-
-        for bucket in crucibleBuckets:
-            if bucket in objective:
-                buckets.append('crucible')
-                break
-
-        for bucket in modesBuckets:
-            if bucket in objective:
-                buckets.append(bucket)
-
-        for mode,bucket in objectiveBasedBuckets.items():
-            if bucket in objective:
-                if mode not in buckets:
-                    buckets.append(mode)
-
-        return buckets
-
-
-    def getGambitBuckets(self, objective):
-
-        gambitBuckets = ['gambit', 'win', 'matches']
-        objectiveBasedBuckets = ['motes']
-        objective = objective.lower()
-
-        buckets = self.getGeneralBuckets(objective)
-
-        for bucket in gambitBuckets:
-            if bucket in objective:
-                buckets.append('gambit')
-                break
-
-        for bucket in objectiveBasedBuckets:
-            if bucket in objective:
-                buckets.append(bucket)
-
-        return buckets
-
-
-    def getLocationBuckets(self, objective):
-
-        locationBuckets = ['chests', 'gather']
-
-        objective = objective.lower()
-
-        buckets = self.getGeneralBuckets(objective)
-
-        for bucket in locationBuckets:
-            if bucket in objective:
-                buckets.append(bucket)
-
-        if 'patrols' in objective:
-            for planet in self.planetBuckets:
-                if planet in objective and planet not in buckets:
-                    buckets.append(planet)
-                    break
-
-        return buckets
-
-
-    def findCommonBuckets(self, activity):
-        
-        bucketsMade = []
-
-        '''first = list(activity.keys())[0]
-
-        firstData = activity.get(first)
-
-        firstBounty = Bounty(first, firstData)
-
-        bucket = Bucket(firstBounty, self.exclusiveData)
-        bucketsMade.append(bucket)
-        del activity[first]'''
-
-        for itemHash, objectives in activity.items():
-            foundValidBucket = False
-            bounty = Bounty(itemHash, objectives)
-            for bucket in bucketsMade:
-                foundValidBucket = bucket.addBounty(bounty)      
-            if not foundValidBucket:
-                bucket = Bucket(bounty, self.exclusiveData)
-                bucketsMade.append(bucket)
-            bucketsMade.sort(key=lambda x: x.size, reverse=False)
-
-        return bucketsMade
         
     def performOptimisation(self, player):
         profile = self.getPlayerData(player)
@@ -181,22 +77,6 @@ class Optimise():
 
         bounties = self.getBounties(items, characters)
         weapons = self.getWeapons(items, characters)
-
-        activityFiltered = self.filterCharacters(bounties)
-
-        crucible = self.findKeyObjectiveData(activityFiltered.get('2305843009301295844').get('crucible'), self.getCrucibleBuckets)
-        gambit = self.findKeyObjectiveData(activityFiltered.get('2305843009301295844').get('gambit'), self.getGambitBuckets)
-        locations = self.findKeyObjectiveData(activityFiltered.get('2305843009301295844').get('locations'), self.getLocationBuckets)
-        other = self.findKeyObjectiveData(activityFiltered.get('2305843009301295844').get('other'), self.getGeneralBuckets)
-
-        logger.info('Crucible objectives: %s', crucible)
-        logger.info('Gambit objectives: %s', gambit)
-        logger.info('Locations objectives: %s', locations)
-        logger.info('Other objectives: %s', other)
-
-        common = self.findCommonBuckets(other)
-
-        logger.info('Common buckets: %s', common)
 
 
     def getAllItems(self, membershipType, membershipId):
@@ -265,81 +145,6 @@ class Optimise():
         displayName = response.get('displayName')
 
         return [pType, pId, displayName]
-
-
-    def filterCharacters(self, bounties:dict):
-        characters = list(bounties.keys())
-
-        filtered = {}
-
-        for character in characters:
-            filtered[character] = self.filter(bounties.get(character))
-
-        return filtered
-
-
-    def filter(self, items:list):
-
-        activities = {'crucible':[], 'gambit':[],'strikes':[], 'locations':[], 'other':[]}
-
-        logger.debug('Filtering items: %s', items)
-
-        for item in items:
-            itemHash = item.get('itemHash')
-            itemInstance = item.get('itemInstanceId')
-            itemStore = [itemHash, itemInstance]
-            itemData = self.getValueFromTable(unHashToId(itemHash), 'DestinyInventoryItemDefinition')
-            stackId = itemData.get('inventory').get('stackUniqueLabel')
-
-            if 'crucible' in stackId or 'pvp' in stackId:
-                activities['crucible'].append(itemStore)
-            elif 'gambit' in stackId:
-                activities['gambit'].append(itemStore)
-            elif 'v400.bounties.destinations' in stackId:
-                activities['locations'].append(itemStore)
-            else:
-                activities['other'].append(itemStore)
-
-            logger.debug('Sorted bounty: %s', item)
-
-        logger.debug('Filtered items: %s', activities)
-        logger.info('Done filtering items')
-        return activities
-
-
-    def sortByLocation(self, locations:dict):
-
-        planets = {}
-
-        for item, objectives in locations.items():
-            planet = objectives[-1]
-            if planet in planets:
-                planets[planet] += objectives[:-1]
-            else:
-                planets[planet] = objectives[:-1]
-
-        return planets
-
-
-    def findKeyObjectiveData(self, bounties:list, optimalMethod):
-
-        bountyObjectives = {}
-
-        for bounty in bounties:
-            itemHash = bounty[0]
-            itemData = self.getValueFromTable(unHashToId(itemHash), 'DestinyInventoryItemDefinition')
-            objectiveHashes = itemData.get('objectives').get('objectiveHashes')
-            bountyObjectives[itemHash] = []
-            for objective in objectiveHashes:
-                #objectiveData = self.getValueFromTable(unHashToId(objective), 'DestinyObjectiveDefinition')
-                #logger.debug('From Objective Data buckets gained: %s', self.getBucketForObjective(objectiveData.get('progressDescription')))
-                objectiveBucket = optimalMethod(itemData.get('displayProperties').get('description'))
-                logger.debug('From Item Data buckets gained: %s', objectiveBucket)
-                bountyObjectives[itemHash].append(objectiveBucket)
-            
-            bountyObjectives[itemHash] = list(chain.from_iterable(bountyObjectives.get(itemHash)))
-
-        return bountyObjectives
 
 
     def getValueFromTable(self, valueId, tableName):
